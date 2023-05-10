49b8b98c9159322ef21edcc91c1602166b52b453
Dmytro Karpenko <kardmitriy@gmail.com>
2023-05-11 00:05:18 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,16 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from './App.styled';
-import { nanoid } from 'nanoid';
-// import FeedbackOptions from './FeedbackOptions';
-// import Statistics from './Statistics';
-// import Section from './Section';
-// import Notification from './Notification';
-// import PropTypes from 'prop-types';
 
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
-const users = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const users = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
@@ -27,14 +21,6 @@ const App = () => {
   const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
-    // window.localStorage.setItem('contacts', JSON.stringify(users));
-
-    // const savedContacts = JSON.parse(window.localStorage.getItem('contacts'));
-    // console.log('savedContacts:', savedContacts);
-
-    // if (savedContacts !== null) {
-    //   console.log('!Null');
-    // }
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
@@ -101,102 +87,102 @@ const App = () => {
     </>
   );
 };
-class OLDApp extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    isActive: true,
-  };
+// class OLDApp extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//     isActive: true,
+//   };
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
+//   componentDidMount() {
+//     const savedContacts = localStorage.getItem('contacts');
+//     if (savedContacts !== null) {
+//       this.setState({ contacts: JSON.parse(savedContacts) });
+//     }
+//   }
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
 
-  addUser = ({ name, number }) => {
-    const user = {
-      id: nanoid(5),
-      name,
-      number,
-    };
+//   addUser = ({ name, number }) => {
+//     const user = {
+//       id: nanoid(5),
+//       name,
+//       number,
+//     };
 
-    const existing = this.state.contacts.find(({ name }) => name === user.name);
+//     const existing = this.state.contacts.find(({ name }) => name === user.name);
 
-    if (existing) {
-      alert(`${user.name} is already in contacts`);
-      return;
-    }
+//     if (existing) {
+//       alert(`${user.name} is already in contacts`);
+//       return;
+//     }
 
-    this.setState(prevState => ({
-      contacts: [user, ...prevState.contacts],
-    }));
-  };
+//     this.setState(prevState => ({
+//       contacts: [user, ...prevState.contacts],
+//     }));
+//   };
 
-  onInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
+//   onInputChange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
 
-  getVisibleItems = () => {
-    return this.state.contacts.filter(el =>
-      el.name
-        .toLocaleLowerCase()
-        .includes(this.state.filter.toLocaleLowerCase())
-    );
-  };
+//   getVisibleItems = () => {
+//     return this.state.contacts.filter(el =>
+//       el.name
+//         .toLocaleLowerCase()
+//         .includes(this.state.filter.toLocaleLowerCase())
+//     );
+//   };
 
-  handleDelete = idUser => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(({ id }) => idUser !== id),
-    }));
-  };
+//   handleDelete = idUser => {
+//     this.setState(({ contacts }) => ({
+//       contacts: contacts.filter(({ id }) => idUser !== id),
+//     }));
+//   };
 
-  onStatusChange = () => {
-    this.setState({ isActive: !this.state.isActive });
-  };
+//   onStatusChange = () => {
+//     this.setState({ isActive: !this.state.isActive });
+//   };
 
-  render() {
-    return (
-      <>
-        <Container>
-          <h2>Phonebook</h2>
+//   render() {
+//     return (
+//       <>
+//         <Container>
+//           <h2>Phonebook</h2>
 
-          <ContactForm onSubmit={this.addUser} />
-        </Container>
+//           <ContactForm onSubmit={this.addUser} />
+//         </Container>
 
-        <Container>
-          <h2>Contacts</h2>
+//         <Container>
+//           <h2>Contacts</h2>
 
-          <Filter
-            onChange={this.onInputChange}
-            value={this.state.filter}
-            isActive={this.state.isActive}
-            onClick={this.onStatusChange}
-          />
-          <ContactList
-            visibleList={this.getVisibleItems()}
-            onDeleteUser={this.handleDelete}
-            isActive={this.state.isActive}
-          />
-        </Container>
-      </>
-    );
-  }
-}
+//           <Filter
+//             onChange={this.onInputChange}
+//             value={this.state.filter}
+//             isActive={this.state.isActive}
+//             onClick={this.onStatusChange}
+//           />
+//           <ContactList
+//             visibleList={this.getVisibleItems()}
+//             onDeleteUser={this.handleDelete}
+//             isActive={this.state.isActive}
+//           />
+//         </Container>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
